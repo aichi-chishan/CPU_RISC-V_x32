aed7a63c29df7b917d5bfa62012196ae3564e733
Yaqian <2741559686@qq.com>
2026-02-05 20:28:23 +0800

    添加了内存模块

---
+++ rtl/data_mem.v
@@ -0,0 +1,15 @@
+module data_mem (
+    input wire clk,
+    input wire we,
+    input wire [31:0] addr,
+    input wire [31:0] wdata,
+    output wire [31:0] rdata
+);
+    reg [31:0] ram [0:1023];
+    assign rdara = ram[addr[31:2]];
+    always @(posedge clk)begin
+        if (we) begin
+            ram[addr[31:2]] <= wdata;
+        end
+    end
+endmodule
