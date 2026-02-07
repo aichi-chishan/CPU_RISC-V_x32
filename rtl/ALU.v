57bbe895a199bd8c2f56002962a87f99d3744110
Yaqian <2741559686@qq.com>
2026-02-07 11:46:01 +0800

    完善了ALU

--- rtl/ALU.v
+++ rtl/ALU.v
@@ -6,15 +6,32 @@ module ALU (
     output reg  [31:0] result,  // 运算结果
     output wire        zero     // 结果是否为0 (分支指令用，当前可留空)
 );
+    parameter ADD  = 4'd0;
+    parameter SUB  = 4'd1; 
+    parameter AND  = 4'd2;
+    parameter OR   = 4'd3;
+    parameter XOR  = 4'd4;
+    parameter SLL  = 4'd5;
+    parameter SRL  = 4'd6;
+    parameter SRA  = 4'd7;
+    parameter SLT  = 4'd8;
+    parameter SLTU = 4'd9;
+
     always @(*) begin
         case(alu_ctrl)
-            4'b0000:begin
-                result = src_a + src_b;
-            end
-            4'b0001:begin
-                result = src_a - src_b;
-            end
-            //更多功能待补充
+            //算术运算
+            ADD:result = src_a + src_b;
+            SUB:result = src_a - src_b;
+            AND:result = src_a & src_b;
+            OR: result = src_a | src_b;
+            XOR:result = src_a ^ src_b;
+            //位移指令
+            SLL:result = src_a << src_b[4:0];
+            SRL:result = src_a >> src_b[4:0];
+            SRA:result = $signed(src_a) >>> src_b[4:0];  
+            //比较指令
+            SLT: result = ($signed(src_a) < $signed(src_b)) ? 32'd1 : 32'd0;
+            SLTU:result = (src_a < src_b) ? 32'd1 : 32'd0;
             default: begin
                 result = 32'b0;  // 默认值
             end
