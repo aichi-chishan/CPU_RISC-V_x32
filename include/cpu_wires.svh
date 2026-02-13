cf7bfdb3a1a059caaf87a7406a613fd1db4b46d2
Yaqian <2741559686@qq.com>
2026-02-13 13:26:42 +0800

    改整齐了一下头文件

--- include/cpu_wires.svh
+++ include/cpu_wires.svh
@@ -58,6 +58,14 @@
     wire ex_mem_we;
     wire ex_branch;
     wire ex_jump;
+    //漏掉的声明
+    wire [3:0] ex_alu_ctrl;
+    wire [31:0] ex_pc;
+    wire [31:0] ex_rdata1;
+    wire [31:0] ex_rdata2;
+    wire [31:0] ex_imm;
+    wire ex_alu_src;
+    wire [2:0] ex_funct3;
 
 //MEM阶段
     wire mem_reg_we;
@@ -88,17 +96,6 @@
     wire [4:0]  wb_waddr;
     wire [31:0] wb_wdata;
 
-//漏掉的声明
-    wire [3:0] ex_alu_ctrl;
-    wire [31:0] ex_pc;
-    wire [31:0] ex_rdata1;
-    wire [31:0] ex_rdata2;
-    wire [31:0] ex_imm;
-    wire [4:0] ex_raddr1;
-    wire [4:0] ex_raddr2;
-    wire ex_alu_src;
-    wire [3:0] ex_alu_ctrl;
-    wire [2:0] ex_funct3;
     
 //Hazard unit 冒险单元
     wire ex_mem_read;
